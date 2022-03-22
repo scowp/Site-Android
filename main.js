49a49d885b9775208392a11c3956b4d5777832a2
scowp <matheusgcampo@gmail.com>
2022-03-22 18:23:29 -0300

    Correção do isflipped do Card

--- main.js
+++ main.js
@@ -3,3 +3,9 @@ const card = document.querySelector(".card__inner");
 card.addEventListener("click", function (e) {
   card.classList.toggle('is-flipped');
 });
+
+const card2 = document.querySelector(".card__inner2");
+
+card2.addEventListener("click", function (e) {
+  card2.classList.toggle('is-flipped');
+});
